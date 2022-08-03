0c6bb04f1c5e7cc106c2109b3c4b1ff869e7d03d
s-h-u-b-s-07 <88794020+s-h-u-b-s-07@users.noreply.github.com>
2022-08-03 10:14:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.get('/', function(req,res){
-        res.send("Hello Everyone");
+        res.send("Hello shubham");
 });
 
 server.listen(3000, function(){
