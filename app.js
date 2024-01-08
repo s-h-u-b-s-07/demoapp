050cd1b5a2b5df0275525caa1b3a7a6cb25a1d9d
shubham17998 <shubhamgawali.sag@gmail.com>
2024-01-08 18:30:05 +0530

    my commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.get('/', function(req,res){
-        res.send("Hello 123..!");
+        res.send("Hello..! everyone..!");
 });
 
 server.listen(3000, function(){
