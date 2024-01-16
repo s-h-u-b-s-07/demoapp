8c6dff1c28839999247426217e4bc72764094d07
shubham-technoforte <125962820+shubham-technoforte@users.noreply.github.com>
2024-01-17 00:08:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.get('/', function(req,res){
-        res.send("Hello....--.!");
+        res.send("Hello--.!");
 });
 
 server.listen(3000, function(){
