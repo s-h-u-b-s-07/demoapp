acb309de41de810b555b081768a439ac3014f030
shubham-technoforte <shubham.gawali@technoforte.co.in>
2024-01-24 12:29:53 +0530

    [JIRA-1234] updated app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.get('/', function(req,res){
-        res.send("Hello--shubham!");
+        res.send("Hello..!");
 });
 
 server.listen(3000, function(){
